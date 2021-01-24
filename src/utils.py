ac98d184d9a0552bb48c2f5435956659399782cc
Jerome Blin <blinjrm@gmail.com>
2021-01-24 22:13:45 +0100

    initiate webapp

---
+++ src/utils.py
@@ -0,0 +1,60 @@
+import requests
+import streamlit as st
+
+
+PROJECT_DESCRIPTION = "Projection desciption"
+
+API_URL = "http://35.240.18.17:8000"
+
+
+class CallAPI:
+    def __init__(self):
+        self.URL = API_URL
+
+    def post(self, input):
+        return requests.post(self.URL, json=input)
+
+    def get(self, input):
+        return requests.get(self.URL, data=input)
+
+
+def form():
+    lead_data = {}
+
+    f1 = "ID_CLIENT"
+    lead_data[f1] = st.text_input(label=f1, value="")
+
+    f2 = "ORIGINE_LEAD"
+    lead_data[f2] = st.text_input(label=f2, value="")
+
+    f3 = "SOURCE_LEAD"
+    lead_data[f3] = st.text_input(label=f3, value="")
+
+    f4 = "CONTACT_PAR_MAIL"
+    lead_data[f4] = st.select_slider(label=f4, options=["non", "oui"])
+
+    f5 = "STATUT_ACTUEL"
+    lead_data[f5] = st.text_input(label=f5, value="")
+
+    f6 = "NB_VISITES"
+    lead_data[f6] = st.number_input(label=f6, min_value=0, step=1)
+
+    f7 = "DUREE_SUR_SITEWEB"
+    lead_data[f7] = st.number_input(label=f7, min_value=0)
+
+    f8 = "DERNIERE_ACTIVITE"
+    lead_data[f8] = st.text_input(label=f8, value="")
+
+    f9 = "VILLE"
+    lead_data[f9] = st.text_input(label=f9, value="")
+
+    f10 = "SPECIALISATION"
+    lead_data[f10] = st.text_input(label=f10, value="")
+
+    f11 = "Comment avez-vous entendu parler de nous ?"
+    lead_data[f11] = st.text_input(label=f11, value="")
+
+    f12 = "Souhaites-tu recevoir une copie de notre livre blanc ?"
+    lead_data[f12] = st.text_input(label=f12, value="")
+
+    return lead_data
---
+++ src/webapp.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import streamlit as st
+
+import src.utils as utils
+from src.utils import CallAPI, form
+
+
+API = CallAPI()
+
+
+def project_description():
+    """Streamlit widget to show the project description on the home page."""
+
+    st.title("Lead scoring")
+    st.text("")
+    st.markdown(utils.PROJECT_DESCRIPTION)
+
+
+def single_prediction():
+    page = st.empty()
+
+    with page.beta_container():
+        with st.beta_expander(label="Form", expanded=True):
+            lead_data = [form()]
+        st.text("")
+        get_pred = st.button("Get prediction")
+
+    if get_pred:
+        with page.beta_container():
+            with st.spinner("Getting predictions..."):
+                response = API.post(lead_data)
+
+            st.subheader("Input:")
+            st.json(lead_data)
+            st.markdown("---")
+
+            st.subheader("Prediction:")
+            if str(response) == "<Response [200]>":
+                result = eval(response.text)
+                st.text(result[0])
+            else:
+                st.text(f"Error {response}: \n{response.text}")
+
+            st.markdown("---")
+            if st.button("New prediction"):
+                single_prediction()
+
+
+def batch_prediction():
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
+
+    # Get predictions
+
+
+def lead_data():
+    id = st.text_input(label="Lead id", value="")
+    # id_dict = {"id": id}
+    if st.button("Get data"):
+        response = API.get(id)
+
+        # df = pd.DataFrame(response.text)
+        st.write(id)
+        st.write(response)
+        st.write(response.text)
+
+
+def documentation():
+    st.title("API documentation")
+    st.text("")
+
+    link = f"[> Access the documentation here <]({utils.API_URL}/docs)"
+    st.markdown(link, unsafe_allow_html=True)
+
+
+def main():
+    """Homepage for the Streamlit app, redirects to appropriate function based on user selection."""
+
+    st.set_page_config(
+        page_title="Lead scoring",
+        page_icon="💯",
+        layout="centered",
+        initial_sidebar_state="auto",
+    )
+
+    st.sidebar.text("")
+    st.sidebar.text("")
+
+    option = st.sidebar.selectbox(
+        "I want to...",
+        [
+            "",
+            "make a single prediction",
+            "make predictions on a file",
+            "get data for a lead",
+            "see the API documentation",
+        ],
+    )
+
+    st.text(" ")
+    st.sidebar.markdown("---")
+
+    if option == "":
+        project_description()
+    elif option == "make a single prediction":
+        single_prediction()
+    elif option == "make predictions on a file":
+        batch_prediction()
+    elif option == "get data for a lead":
+        lead_data()
+    else:
+        documentation()
+
+
+if __name__ == "__main__":
+    main()
